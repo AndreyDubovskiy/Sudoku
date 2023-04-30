4f631dc793956ca79dcf4b9d61e272566a98916c
AndreyWarGold-coder <alaxbabax12g@gmail.com>
2023-04-30 19:12:52 +0300

    add test.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/index', to: 'sudokus#index'
-  #afdasfadsfdasf
+  get '/test', to: 'sudokus#test'
   get '/index2', to: 'tests#index'
   get 'sudoku/index'
   patch 'sudoku/update'
